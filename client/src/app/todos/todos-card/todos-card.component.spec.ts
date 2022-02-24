764fd7caebc5744f7456a300c057f586bac16c77
Collin Beane <beane039@morris.umn.edu>
2022-02-24 15:16:55 -0600

    Add todo-card-component code
    Adds code for all parts of the todo-card component, including tests in the spec.ts

--- client/src/app/todos/todos-card/todos-card.component.spec.ts
+++ client/src/app/todos/todos-card/todos-card.component.spec.ts
@@ -1,21 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TodosCardComponent } from './todos-card.component';
+import { MatCardModule } from '@angular/material/card';
 
 describe('TodosCardComponent', () => {
   let component: TodosCardComponent;
   let fixture: ComponentFixture<TodosCardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync (() => {
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserAnimationsModule,
+        MatCardModule
+      ],
       declarations: [ TodosCardComponent ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodosCardComponent);
     component = fixture.componentInstance;
+    component.todos = {
+      _id: 'sally_id',
+      owner: 'Sally',
+      status: false,
+      category: 'groceries',
+      body: 'Sallys body of her todo'
+    };
     fixture.detectChanges();
   });
 
