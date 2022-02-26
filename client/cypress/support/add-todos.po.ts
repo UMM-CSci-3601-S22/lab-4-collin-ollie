749fa6a9bc08da4611efc784a6dc362e46121ff8
Collin Beane <beane039@morris.umn.edu>
2022-02-26 16:32:24 -0600

    Add "add-todos.po.ts" file
    adds the cypress file to support the tests for add-todos.

---
+++ client/cypress/support/add-todos.po.ts
@@ -0,0 +1,38 @@
+import { Todos } from 'src/app/todos/todos';
+
+export class AddTodosPage {
+  navigateTo() {
+    return cy.visit('/todos/new');
+  }
+
+  getTitle() {
+    return cy.get('.add-todos-title');
+  }
+
+  addTodosButton() {
+    return cy.get('[data-test=confirmAddTodosButton]');
+  }
+
+  selectMatSelectValue(select: Cypress.Chainable, value: string) {
+    return select.click()
+      .get(`mat-option[value="${value}:"]`).click();
+  }
+
+  getFormField(fieldName: string) {
+    return cy.get(`mat-form-field [formcontrolname=${fieldName}]`);
+  }
+
+  addTodos(newTodo: Todos) {
+    this.getFormField('owner').type(newTodo.owner);
+    this.getFormField('body').type(newTodo.body);
+    this.getFormField('category').type(newTodo.category);
+    if(newTodo.status) {
+      this.selectMatSelectValue(this.getFormField('status'), 'Complete');
+    }
+    if(!newTodo.status) {
+      this.selectMatSelectValue(this.getFormField('status'), 'Incomplete');
+    }
+    return this.addTodosButton().click();
+  }
+
+}
